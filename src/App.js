204e7547cac5bfceb6262271adc57eabfa01ce86
guri <gurmeetguri16124@gmail.com>
2022-08-11 00:00:46 +0530

    gurigit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          my reacter kjvkskdfkj
+          my name is gurmeet singh 
         </p>
         <a
           className="App-link"
