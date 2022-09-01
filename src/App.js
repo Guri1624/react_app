b74e24663968542f599a353c3d934e6af9523231
guri <gurmeetguri16124@gmail.com>
2022-09-01 11:25:06 +0530

    uncontrolefd componennt

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react'
+import React, { useRef } from 'react'
 function  App ()
-{
-  const [val,setval]=useState("000");
-  const [item,setitem]=useState();
- 
+{ 
+  let inputRef=useRef(null)
+  let inputRef2=useRef(null)
+  
+  function submitform(e)
+  {
+e.preventDefault()
+console.warn("input filg value=",inputRef.current.value)
+console.warn("input filg value2=",inputRef2.current.value)
+let input3=document.getElementById('input3').value
+console.warn("input  fild value3",input3)
+  }
       return (
       <div className='App' >   
-             <h1>controlled statemnt</h1> 
-             <input type="text" value={val} onChange={(e)=>setval(e.target.value)}/>
-             <h3>show value= {val}</h3>
+             <h1>uncontroled component</h1> 
+             <form onSubmit={submitform}>
+             <input ref={inputRef} type="text"/><br></br>
+             <input ref={inputRef2}type="text"/><br></br>
+             <input id="input3" type="text"/><br></br>
+             <button>click me </button><br></br>
+             </form>
      </div>
-     
             );
   }
 
