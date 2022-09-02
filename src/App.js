e30f5d6e6a85c7eae8d9fa7b21dae530ce51db42
guri <gurmeetguri16124@gmail.com>
2022-09-02 12:52:55 +0530

    style method rout

--- src/App.js
+++ src/App.js
@@ -4,19 +4,17 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './componenet/Home'
 import Rout2 from './componenet/Rout2'
 import Error from './componenet/Error'
+import Navbar from './componenet/Navbar'
 function App() {
   return (
     <div className='App'>
       <BrowserRouter>
-      <Link to="/home">home !</Link><br></br>
-     <Link to="/rout2">rout2 !</Link><br></br>
-     <Link to="/Error/anil">anil!</Link><br></br>
-     <Link to="/Error/peter">peter</Link><br></br>
+        <Navbar />
         <Routes>
           <Route path="/home" element={<Home />} />
-          <Route path="/rout2" element={<Rout2/>} />
-          <Route path="/Error/:name"   element={<Error/>}/>
-          <Route path="/"  element={<Error/>}/>
+          <Route path="/rout2" element={<Rout2 />} />
+          <Route path="/Error/:name" element={<Error />} />
+          <Route path="/" element={<Error />} />
         </Routes>
       </BrowserRouter>
     </div>
